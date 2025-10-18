04fe822592de1e6e7cfe60320ed706fff73d0e86
RobertoJavaDev <robertojavadev@gmail.com>
2025-10-18 13:49:22 +0200

    Initial commit

---
+++ src/main/java/com/robertojavadev/cadency/CadencyApplication.java
@@ -0,0 +1,13 @@
+package com.robertojavadev.cadency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robertojavadev/cadency/CadencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertojavadev.cadency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
