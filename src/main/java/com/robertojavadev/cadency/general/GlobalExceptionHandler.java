7e94d1dbc80a9eaa847916a392c6bc97054c7bc0
RobertoJavaDev <robertojavadev@gmail.com>
2025-10-23 08:37:07 +0200

    feat: Implemented method argument not valid exception handler

--- src/main/java/com/robertojavadev/cadency/general/GlobalExceptionHandler.java
+++ src/main/java/com/robertojavadev/cadency/general/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.robertojavadev.cadency.general;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,6 +25,14 @@ class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleConstraintViolationException(ConstraintViolationException e) {
         log.error("ConstraintViolationException: {}", e.getMessage());
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        e.getBindingResult().getFieldErrors().forEach(m ->
+                log.error("MethodArgumentNotValidException in: {}", m.getField()));
         return e.getMessage();
     }
 }
